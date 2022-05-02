f0618735ba0110197085a2a1b8e4a2126c4b84de
rouge9 <robelged1992@gmail.com>
2022-05-02 02:42:22 -0900

    post page doesn't work

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
-
+import requests
+posts = requests.get("https://api.npoint.io/910c59e009295c7ab2e8").json()
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', all_posts=posts)
 
 
 @app.route('/about')
@@ -13,9 +14,14 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/post')
-def post():
-    return render_template('post.html')
+@app.route('/post/<int:index>')
+def show_post(index=None):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post['id'] == index:
+            requested_post = blog_post
+
+    return render_template("post.html", post=requested_post)
 
 
 @app.route('/contact')
